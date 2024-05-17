80cc4d828c6638e004529ca881a7d709f775403c
wyf-ACCEPT <43724111+wyf-ACCEPT@users.noreply.github.com>
2024-05-17 23:17:09 +0800

    clean: code cleaning, update interface

--- contracts/interface/interface.sol
+++ contracts/interface/interface.sol
@@ -26,8 +26,10 @@ interface BondingsCoreInterface {
     event LaunchBondings(
         uint256 bondingsId,
         string bondingsName,
+        string bondingsSymbol,
         address indexed user
     );
+    event OperatorSet(address operator, bool isOperator_);
     event OwnershipTransferStarted(
         address indexed previousOwner,
         address indexed newOwner
@@ -35,6 +37,14 @@ interface BondingsCoreInterface {
     event OwnershipTransferred(
         address indexed previousOwner,
         address indexed newOwner
+    );
+    event RetrieveAndDeploy(
+        uint256 bondingsId,
+        string bondingsName,
+        string bondingsSymbol,
+        uint256 finalSupply,
+        address operator,
+        address tokenAddress
     );
     event SellBondings(
         uint256 bondingsId,
@@ -46,13 +56,6 @@ interface BondingsCoreInterface {
         uint256 sellPriceAfterFee,
         uint256 fee
     );
-    event TransferBondings(
-        uint256 bondingsId,
-        string bondingsName,
-        address indexed from,
-        address indexed to,
-        uint256 share
-    );
 
     function acceptOwnership() external;
 
@@ -61,6 +64,12 @@ interface BondingsCoreInterface {
     function bondingsName(uint256) external view returns (string memory);
 
     function bondingsStage(uint256) external view returns (uint8);
+
+    function bondingsSymbol(uint256) external view returns (string memory);
+
+    function bondingsTokenAddress(uint256) external view returns (address);
+
+    function bondingsTokenSupply() external view returns (uint256);
 
     function bondingsTotalShare(uint256) external view returns (uint256);
 
@@ -111,7 +120,7 @@ interface BondingsCoreInterface {
 
     function isOperator(address) external view returns (bool);
 
-    function launchBondings(string memory name) external;
+    function launchBondings(string memory name, string memory symbol) external;
 
     function maxSupply() external view returns (uint256);
 
@@ -127,11 +136,15 @@ interface BondingsCoreInterface {
 
     function renounceOwnership() external;
 
+    function retrieveAndDeploy(uint256 bondingsId) external;
+
     function sellBondings(
         uint256 bondingsId,
         uint256 share,
         uint256 minGetTokenAmount
     ) external;
+
+    function setBondingsTokenSupply(uint256 newBondingsTokenSupply) external;
 
     function setFairLaunchSupply(uint256 newFairLaunchSupply) external;
 
@@ -141,16 +154,12 @@ interface BondingsCoreInterface {
 
     function setMintLimit(uint256 newMintLimit) external;
 
+    function setOperator(address operator, bool isOperator_) external;
+
     function setProtocolFeeDestination(address newProtocolFeeDestination)
         external;
 
     function setProtocolFeePercent(uint256 newProtocolFeePercent) external;
-
-    function transferBondings(
-        uint256 bondingsId,
-        address to,
-        uint256 share
-    ) external;
 
     function transferOwnership(address newOwner) external;
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,45 +4,22 @@ import "@openzeppelin/hardhat-upgrades";
 import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   etherscan: {
     apiKey: {
-      blast_sepolia: "blast_sepolia", // apiKey is not required, just set a placeholder
       base: process.env.BASE_SCAN_API!,
       sepolia: process.env.ETHER_SCAN_API!,
     },
-    customChains: [
-      {
-        network: "blast_sepolia",
-        chainId: 168587773,
-        urls: {
-          // 1. For Explorer: https://testnet.blastscan.io/
-          // apiURL: "https://api.routescan.io/v2/network/testnet/evm/168587773/etherscan",
-          // browserURL: "https://testnet.blastscan.io"
-          // 2. For Explorer: https://sepolia.blastscan.io/ (Etherscan version)
-          apiURL: "https://api-sepolia.blastscan.io/api",
-          browserURL: "https://sepolia.blastscan.io/"
-        }
-      }
-    ]
   },
   networks: {
-    polygon: {
-      url: process.env.POLYGON_RPC,
-      accounts: [
-        process.env.PRIVATE_KEY_ADMIN!,
-      ]
-      // gas: "auto",
-      // gasPrice: 150000000000, // 150Gwei
-    },
-    blast_sepolia: {
-      // url: process.env.BLAST_TEST_RPC,
-      url: 'https://sepolia.blast.io',
-      chainId: 168587773,
-      accounts: [
-        process.env.PRIVATE_KEY_ADMIN!,
-      ]
-    },
     base: {
       url: process.env.BASE_RPC,
       accounts: [
@@ -59,7 +36,3 @@ const config: HardhatUserConfig = {
 };
 
 export default config;
-
-// r = json.load(open('artifacts/build-info/45b22d3bce577f28d713c4db698357da.json'))
-// s = json.load(open('artifacts/build-info/compare.json'))
-// r['output']['contracts']['contracts/mock/MockUSDB.sol']['MockUSDB']['metadata'] = s['metadata']
